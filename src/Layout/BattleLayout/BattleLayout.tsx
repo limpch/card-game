0edcaf4fc1da74e29fe58f9a0fdc87e9810fd5a8
convertme <convertme.ru@gmail.com>
2024-12-16 19:38:12 +0300

    start do cards holder logic

--- src/Layout/BattleLayout/BattleLayout.tsx
+++ src/Layout/BattleLayout/BattleLayout.tsx
@@ -1,7 +1,7 @@
-import { FC } from 'react';
-import classes from './styles.module.scss';
-import { ICardsMock } from '@/types/game';
-import { CardsHolder } from '@/components/game/CardsHolder';
+import { FC } from "react"
+import classes from "./styles.module.scss"
+import { ICardsMock } from "@/types/game"
+import { CardsHolder } from "@/components/game/CardsHolder"
 
 interface BattleLayoutProps {}
 
@@ -9,97 +9,97 @@ const cardsMock: ICardsMock[] = [
 	{
 		id: 0,
 		data: {
-			type: 'hit',
+			type: "hit",
 			points: 10,
 			stats: [
-				{ type: 'dot', value: 3 },
-				{ type: 'heal', value: 2 },
+				{ type: "dot", value: 3 },
+				{ type: "heal", value: 2 },
 			],
 		},
 	},
 	{
 		id: 1,
 		data: {
-			type: 'hit',
+			type: "hit",
 			points: 10,
 			stats: [
-				{ type: 'dot', value: 3 },
-				{ type: 'heal', value: 2 },
+				{ type: "dot", value: 3 },
+				{ type: "heal", value: 2 },
 			],
 		},
 	},
 	{
 		id: 2,
 		data: {
-			type: 'hit',
+			type: "hit",
 			points: 10,
 			stats: [
-				{ type: 'dot', value: 3 },
-				{ type: 'heal', value: 2 },
+				{ type: "dot", value: 3 },
+				{ type: "heal", value: 2 },
 			],
 		},
 	},
-	{
-		id: 3,
-		data: {
-			type: 'hit',
-			points: 10,
-			stats: [
-				{ type: 'dot', value: 3 },
-				{ type: 'heal', value: 2 },
-			],
-		},
-	},
-	{
-		id: 4,
-		data: {
-			type: 'hit',
-			points: 10,
-			stats: [
-				{ type: 'dot', value: 3 },
-				{ type: 'heal', value: 2 },
-			],
-		},
-	},
-	{
-		id: 5,
-		data: {
-			type: 'hit',
-			points: 10,
-			stats: [
-				{ type: 'dot', value: 3 },
-				{ type: 'heal', value: 2 },
-			],
-		},
-	},
-	{
-		id: 6,
-		data: {
-			type: 'heal',
-			points: 5,
-			stats: [
-				{ type: 'dot', value: 3 },
-				{ type: 'heal', value: 2 },
-			],
-		},
-	},
-	{
-		id: 7,
-		data: {
-			type: 'dot',
-			points: 10,
-			stats: [
-				{ type: 'heal', value: 3 },
-				{ type: 'cooldown', value: 2 },
-			],
-		},
-	},
-];
+	// {
+	// 	id: 3,
+	// 	data: {
+	// 		type: "hit",
+	// 		points: 10,
+	// 		stats: [
+	// 			{ type: "dot", value: 3 },
+	// 			{ type: "heal", value: 2 },
+	// 		],
+	// 	},
+	// },
+	// {
+	// 	id: 4,
+	// 	data: {
+	// 		type: "hit",
+	// 		points: 10,
+	// 		stats: [
+	// 			{ type: "dot", value: 3 },
+	// 			{ type: "heal", value: 2 },
+	// 		],
+	// 	},
+	// },
+	// {
+	// 	id: 5,
+	// 	data: {
+	// 		type: "hit",
+	// 		points: 10,
+	// 		stats: [
+	// 			{ type: "dot", value: 3 },
+	// 			{ type: "heal", value: 2 },
+	// 		],
+	// 	},
+	// },
+	// {
+	// 	id: 6,
+	// 	data: {
+	// 		type: "heal",
+	// 		points: 5,
+	// 		stats: [
+	// 			{ type: "dot", value: 3 },
+	// 			{ type: "heal", value: 2 },
+	// 		],
+	// 	},
+	// },
+	// {
+	// 	id: 7,
+	// 	data: {
+	// 		type: "dot",
+	// 		points: 10,
+	// 		stats: [
+	// 			{ type: "heal", value: 3 },
+	// 			{ type: "cooldown", value: 2 },
+	// 		],
+	// 	},
+	// },
+]
 
 export const BattleLayout: FC<BattleLayoutProps> = ({}) => {
 	return (
 		<div className={classes.container}>
 			<CardsHolder cardsMock={cardsMock} />
 		</div>
-	);
-};
+	)
+}
--- src/components/game/CardsHolder/CardsHolder.tsx
+++ src/components/game/CardsHolder/CardsHolder.tsx
@@ -1,35 +1,79 @@
-import { FC } from 'react';
-import classes from './styles.module.scss';
-import { Card } from '@/components/game/Card';
-import { ICardsMock } from '@/types/game';
+import { FC, useEffect, useMemo, useState } from "react"
+import classes from "./styles.module.scss"
+import { Card } from "@/components/game/Card"
+import { ICardsMock } from "@/types/game"
+import { useElementSize } from "@mantine/hooks"
 
 interface CardsHolderProps {
-	cardsMock: ICardsMock[];
+	cardsMock: ICardsMock[]
 }
 
 export const CardsHolder: FC<CardsHolderProps> = ({ cardsMock }) => {
+	const [cards, setCards] = useState<ICardsMock[]>(cardsMock)
+
+	useEffect(() => {
+		const handleKeyDown = (e: KeyboardEvent) => {
+			if (e.key === "ArrowUp") {
+				setCards(prev => [...prev, cardsMock[0]])
+			} else if (e.key === "ArrowDown") {
+				setCards(prev => prev.slice(1))
+			}
+		}
+
+		window.addEventListener("keydown", handleKeyDown)
+
+		return () => {
+			window.removeEventListener("keydown", handleKeyDown)
+		}
+	}, [cardsMock])
+
+	const { ref, width } = useElementSize<HTMLDivElement>()
+
+	const offset = useMemo(() => {
+		if (!ref.current) return { x: 0, y: 8, container: 0 }
+		const container = ref.current as HTMLDivElement
+
+		const cardWidth = 90
+		const containerWidth = width
+		const baseOffset = 20
+		const cardsCount = cards.length
+		const cardsWidth = cardsCount * cardWidth
+
+		console.log(90 - (containerWidth - cardWidth) / cardsCount)
+
+		if (cardsWidth >= containerWidth) {
+			const x = cardWidth - (containerWidth - cardWidth) / cardsCount
+
+			container.classList.add(classes.justify_start)
+
+			return { x, y: 8, container: 0 }
+		}
+
+		container.classList.remove(classes.justify_start)
+		return { x: baseOffset, y: 8, container: baseOffset }
+	}, [cards.length, width])
+
 	return (
 		<div
 			className={classes.holder}
 			// style={{ maxWidth: cardsMock.length * 70 + 40 }}
 		>
-			<div className={classes.holder__container}>
-				{cardsMock.map(cardMock => (
+			<div
+				className={classes.holder__container}
+				ref={ref}
+				style={{ transform: `translateX(${offset.container}px)` }}
+			>
+				{cards.map((cardMock, _ind) => (
 					<div
 						className={classes.holder__item}
 						style={{
-							transform: `translateY(${cardMock.id * 8}px) translateX(${
-								cardMock.id * 34
-							}px) skewY(-8deg)`,
+							transform: `translateY(${_ind * offset.y}px) translateX(${_ind * -offset.x}px) `,
 						}}
 					>
-						<Card
-							key={cardMock.id}
-							data={cardMock.data}
-						/>
+						<Card key={_ind} data={cardMock.data} />
 					</div>
 				))}
 			</div>
 		</div>
-	);
-};
+	)
+}
